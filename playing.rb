aa5f1968fc0249040125ca4a9579f8eb921c446f
Ryan Melanson <melansonryanf@gmail.com>
2016-11-11 01:18:43 +0000

    First work with RDB and Active Record

---
+++ playing.rb
@@ -0,0 +1,14 @@
+def telephone(x)
+    if x == "A"
+        puts "2"
+    else puts "wrong"
+    end
+end
+
+puts "Give me a capital letter"
+    letter = gets.chomp.to_i
+    
+telephone(letter) 
+    
+
+    
