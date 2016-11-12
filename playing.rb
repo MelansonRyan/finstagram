a37d62a5923f3e8b1bcf4a96ab2fc605cb3c1d79
Ryan Melanson <melansonryanf@gmail.com>
2016-11-12 00:58:48 +0000

    trying to make google fonts work

--- playing.rb
+++ playing.rb
@@ -1,14 +1,3 @@
-def telephone(x)
-    if x == "A"
-        puts "2"
-    else puts "wrong"
-    end
-end
 
-puts "Give me a capital letter"
-    letter = gets.chomp.to_i
-    
-telephone(letter) 
-    
 
     
